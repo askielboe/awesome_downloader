381adde0b4b62614d38e57f0ecc04249fa8cb154
Andreas Skielboe <skielboe@gmail.com>
2013-02-15 21:18:49 +0100

    Adding python utility functions.

---
+++ awesome_utility.py
@@ -0,0 +1,33 @@
+def getSession():
+    #--------------------------------------------------------------------------------
+    # Connect to local database
+    #--------------------------------------------------------------------------------
+    from database_operations import create_session
+    session = create_session()
+    return session
+
+def getMovie(imdbid):
+    from Movie import Movie
+    session = getSession()
+    
+    movie = session.query(Movie).filter(Movie.imdbId == imdbid).all()
+    
+    if len(movie) > 0:
+        m = movie[0]
+        print "Found movie: "+m.title+" ("+str(m.year)+") - searched: "+str(m.last_searched)+" - downloaded: "+str(m.downloaded)
+    else:
+        print "No movies found.."
+
+def setNotDownloaded(imdbid):
+    from Movie import Movie
+    session = getSession()
+    movie = session.query(Movie).filter(Movie.imdbId == imdbid).all()
+    if len(movie) > 0:
+        m = movie[0]
+        print "Found movie: "+m.title+" ("+str(m.year)+") - searched: "+str(m.last_searched)+" - downloaded: "+str(m.downloaded)
+        print "Setting downloaded to FALSE.."
+        m.downloaded = 0
+        session.add(m)
+        session.commit()
+    else:
+        print "No movies found.."
