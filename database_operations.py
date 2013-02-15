680f6004eaa2c63c5d458bf83e17c8f298e2d72f
Andreas Skielboe <skielboe@gmail.com>
2013-02-15 18:20:56 +0100

    Fixed settings bug in database operations.

--- database_operations.py
+++ database_operations.py
@@ -9,12 +9,14 @@
 #   Functions that connect to and modify the database at the lowest levels in SQLalchemy
 # 
 
+import settings as s
+
 def create_session():
     #--------------------------------------------------------------------------------
     # Define database
     #--------------------------------------------------------------------------------
     from sqlalchemy import create_engine
-    engine = create_engine('sqlite://'+dbPath)
+    engine = create_engine('sqlite://'+s.dbPath)
     
     #--------------------------------------------------------------------------------
     # Create a session to start talking to the database
