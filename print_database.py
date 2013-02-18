4e5a56108aacdd55764a0a52b365af2fc08582cc
Andreas Skielboe <skielboe@gmail.com>
2013-02-19 00:01:56 +0100

    Work on database print script.

--- print_database.py
+++ print_database.py
@@ -8,11 +8,24 @@ session = create_session()
 # Query database
 #--------------------------------------------------------------------------------
 from Movie import Movie
-movies = session.query(Movie).order_by(Movie.title).all()
+movies = session.query(Movie).filter(Movie.downloaded == 0).order_by(Movie.title).all()
 
 #--------------------------------------------------------------------------------
 # Print output
 #--------------------------------------------------------------------------------
+# Get current date and time
+import time,datetime
+timeNow = datetime.datetime.now()
+timePosix = int(time.mktime(timeNow.timetuple()))
+
 print "<--- Movies --->"
 for movie in movies:
-    print movie.title+' ('+movie.year+')'+' '*(50-len(movie.title))+movie.imdbId+' ', movie.last_searched, ' ', movie.downloaded
+    
+    hoursToNextSearch = int((movie.last_searched - timePosix)/3600.0)
+    
+    if movie.last_searched == 0:
+        string = movie.title+' ('+str(movie.year)+')'+' '*(50-len(movie.title))+movie.imdbId+' '+str(movie.last_searched)+'\t\t'+'\t'+str(movie.downloaded)
+    else: 
+    	string = movie.title+' ('+str(movie.year)+')'+' '*(50-len(movie.title))+movie.imdbId+' '+str(movie.last_searched)+'\t'+str(hoursToNextSearch)+'\t'+str(movie.downloaded)
+    
+    print string
