015023d60ad510b45eebedd71f7748f137e22412
Andreas Skielboe <askielboe@Andreass-MacBook-Pro.local>
2013-04-29 18:55:37 +0200

    Sending push notification for low disk space (only once).

--- run_downloader.py
+++ run_downloader.py
@@ -1,13 +1,28 @@
 #!/opt/bin/python2.6
 
-# Abort if diskspace is below 100 GB
+# Abort if diskspace is below limit set in settings.py
 import os
 import sys
+from pushover import pushover
 from settings import torrentPath, minDiskSpace
 
 s = os.statvfs(torrentPath)
-if (s.f_bavail * s.f_frsize / 1e9) < minDiskSpace:
+spaceLeft = (s.f_bavail * s.f_frsize / 1e9)
+if spaceLeft < minDiskSpace:
+	f = open('.isdiskfull', 'r')
+	if f.readline() == '0':
+		pushover('Awesome-DL','WARNING: Low disk space: '+str(spaceLeft)+' GB. Suspending downloads..')
+		f.close()
+		f = open('.isdiskfull', 'w')
+		f.write('1')
+		f.close()
+
 	sys.exit("FATAL: Disk space too low (< "+str(minDiskSpace)+" GB)!")
+
+
+f = open('.isdiskfull', 'w')
+f.write('0')
+f.close()
 
 # Run downloader
 from imdb_parser import imdbParse
