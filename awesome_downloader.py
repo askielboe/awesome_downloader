31c1179ec788c865e6c13389cb0a659df177c4db
Andreas Skielboe <askielboe@Andreass-MacBook-Pro.local>
2013-05-04 14:36:02 +0200

    Max number of searches in a single session now set in settings file.

--- awesome_downloader.py
+++ awesome_downloader.py
@@ -10,35 +10,35 @@ from pushover import pushover
 def doLogin():
     url = "https://awesome-hd.net/login.php"
     response = mechanize.urlopen(url)
-    
+
     forms = mechanize.ParseResponse(response, backwards_compat=False)
     form = forms[0]
     form["username"] = s.username
     form["password"] = s.password
-    
+
     mechanize.urlopen(form.click())
 
 def doSearch(movieTitle, movieYear):
     # Convert non-unicode characters
     movieTitle = removeNonUnicodeChars(movieTitle)
-    
+
     url = "https://awesome-hd.net/torrents.php"
     response = mechanize.urlopen(url)
-    
+
     # Search
     forms = mechanize.ParseResponse(response, backwards_compat=False)
     form = forms[0]
     form["searchstr"] = movieTitle
-    
+
     html = mechanize.urlopen(form.click()).readlines()
-    
+
     return html
 
 def getLink(html, movieTitle, movieYear):
     # Convert non-unicode characters
     movieTitle = removeNonUnicodeChars(movieTitle)
     movieTitle = movieTitle.lstrip('The ')
-    
+
     # Make sure movie year is an int
     try:
         movieYear = int(movieYear)
@@ -56,7 +56,7 @@ def getLink(html, movieTitle, movieYear):
                     and 'Blu-Ray' in html[i] \
                     and '75% Freeleech' in html[i] \
                     and 'Remux' not in html[i]:
-                    
+
                     link = 'https://awesome-hd.net/'+html[i-3].lstrip('\t\t\t\t[<a href="').rstrip('" title="Download">DL</a>\n')
                     link = ''.join(link.split('amp;'))
                 if '<td colspan="2">' in html[i]:
@@ -79,7 +79,7 @@ def awesomeDownloader():
     import time,datetime
     timeNow = datetime.datetime.now()
     timePosix = int(time.mktime(timeNow.timetuple()))
-    
+
     print "<--------------- Running Downloader ----------------->"
     print "Time: ",timeNow.strftime('%d/%m/%Y %H:%M')
     #--------------------------------------------------------------------------------
@@ -87,26 +87,26 @@ def awesomeDownloader():
     #--------------------------------------------------------------------------------
     from database_operations import create_session
     session = create_session()
-    
+
     #--------------------------------------------------------------------------------
     # Check if we need to search for movies
     #--------------------------------------------------------------------------------
-    
+
     from Movie import Movie
     nDownloaded = session.query(Movie).filter(Movie.downloaded == 1).count()
     nRecent = session.query(Movie).filter(Movie.last_searched+86400 > timePosix).count()
-    
+
     # Get movies we need to search for
-    movies = session.query(Movie).filter(Movie.downloaded == 0).filter(Movie.last_searched+86400 < timePosix).limit(5).all()
-    
+    movies = session.query(Movie).filter(Movie.downloaded == 0).filter(Movie.last_searched+86400 < timePosix).limit(s.maxSearches).all()
+
     nSnatched = 0
     if len(movies) > 0:
         #--------------------------------------------------------------------------------
         # Login to Awesome-HD and search for movies
         #--------------------------------------------------------------------------------
-        
+
         doLogin()
-        
+
         for movie in movies:
             print "---> SEARCHING FOR: "+movie.title.encode('utf8')+' ('+str(movie.year)+')'
             html = doSearch(movie.title, movie.year)
@@ -130,18 +130,18 @@ def awesomeDownloader():
                     print "ERROR: Something went wrong in twill.."
             else:
                 print "NO RESULTS FOR: "+movie.title.encode('utf8')+' ('+str(movie.year)+')'
-            
+
             # Log time of search and add random interval +/- 2 hours to spread out searches
             try:
                 movie.last_searched = timePosix + abs(movie.year - timeNow.year)*86400 + int(random.uniform(-7200,7200))
             except TypeError:
                 movie.last_searched = timePosix + 30*86400 + int(random.uniform(-7200,7200))
-            
+
             session.add(movie)
             session.commit()
-        
+
         session.commit()
-    
+
     print "======================================================"
     print "No. movies already downloaded: ", nDownloaded
     print "No. movies recently searched: ", nRecent
